939fd4426ce234ec00978ec4a182ebb77e6fae77
Vergiet <djhhoornweg@hotmail.com>
2025-05-29 14:08:02 +0200

    add initial player movement

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import pygame
 # from database import connect_database, database_version
 
 from constants import *
+from player import Player
 
 
 
@@ -20,14 +21,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while (pygame.get_init()):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(pygame.Color(color_value))
+        screen.fill(pygame.Color(background_color_value))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60)/1000)
 
 
 
